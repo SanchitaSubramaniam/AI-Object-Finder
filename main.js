72840b43f83748f31d4608d42349cf795d583057
SanchitaSubramaniam <86506500+SanchitaSubramaniam@users.noreply.github.com>
2022-07-29 17:35:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var status = "";
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(480, 380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    document.getElementById("text_input").value = value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
