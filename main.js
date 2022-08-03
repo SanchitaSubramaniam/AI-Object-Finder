c0cded5ce4b95733de05e814a3ce786aebcc2f70
SanchitaSubramaniam <86506500+SanchitaSubramaniam@users.noreply.github.com>
2022-08-03 11:48:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var status = "";
+objects = [];
+status = "";
+video = "";
 
 function setup() {
     canvas = createCanvas(480, 380);
@@ -21,4 +23,33 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_object").innerHTML = "Number of objects detected are: " + objects.length;
+
+            fill('#FF0000');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+        if (objects[i].label == object_name) {
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("status").innerHTML = text + "Found";
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
